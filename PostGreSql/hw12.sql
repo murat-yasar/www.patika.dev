8f81f3b2527bb75ad7a968e6e3df85db551b2f30
Murat Yasar <127419303+murat-yasar@users.noreply.github.com>
2023-09-21 18:51:09 +0200

    Add files via upload

---
+++ PostGreSql/hw12.sql
@@ -0,0 +1,7 @@
+1. SELECT * FROM film WHERE length > (SELECT AVG(length) FROM film);
+
+2. SELECT COUNT(*) FROM film WHERE rental_rate = (SELECT MAX(rental_rate) FROM film);
+
+3. SELECT * FROM film WHERE rental_rate = (SELECT MIN(rental_rate) FROM film) AND replacement_cost = (SELECT MIN(replacement_cost) FROM film);
+
+4. SELECT customer_id, COUNT(payment_id) FROM payment GROUP BY customer_id ORDER BY COUNT(payment_id) DESC LIMIT 1;
